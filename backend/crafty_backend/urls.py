12dbc1e2fef7734c03ae16c5f2b2794dbcad8700
Sonu Kumar <deepakkumar23887@gmail.com>
2025-11-14 15:18:56 +0530

    add paths

--- backend/crafty_backend/urls.py
+++ backend/crafty_backend/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("accounts/", include("userls.urls")),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("seller/dashboard/", TemplateView.as_view(template_name="seller/dashboard.html"), name="seller_dashboard"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
