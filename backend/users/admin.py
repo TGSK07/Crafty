8b6acd60bd7bc9d196ceeb7ac972a2fb2b04cc63
Sonu Kumar <deepakkumar23887@gmail.com>
2025-11-14 15:54:44 +0530

    register the user

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {"fields": ("user_type",)}),
+    )
+    list_display = ("username", "email", "first_name", "last_name", "user_type", "is_staff")
