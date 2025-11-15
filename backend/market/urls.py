1a47d78d26c2e5c5a29c7fe6216d9b99c0e32bb0
Sonu Kumar <deepakkumar23887@gmail.com>
2025-11-15 12:15:44 +0530

    add all the seller product urls

---
+++ backend/market/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import ProductListView, ProductDeleteView, SellerProductsView, ProductCreateView, ProductUpdateView, ProductDetailView
+
+urlpatterns = [
+    path("", ProductListView.as_view(), name="product_list"),
+    path("product/<slug:slug>", ProductDetailView.as_view(), name="product_detail"),
+
+    # Seller's Dashboard
+    path("seller/products/", SellerProductsView.as_view(), name="seller_products"),
+    path("seller/products/create/", ProductCreateView.as_view(), name="product_create"),
+    path("seller/products/<int:pk>/edit/", ProductUpdateView.as_view(), name="product_edit"),
+    path("seller/products/<int:pk>/delete/", ProductDeleteView.as_view(), name="product_delete"),
+]
+
