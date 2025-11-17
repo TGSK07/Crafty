7061a5102b3d9db6379dddfbc085182d7e4fd00d
Sonu Kumar <deepakkumar23887@gmail.com>
2025-11-17 06:48:18 +0530

    Link the new views urls

--- backend/crafty_backend/urls.py
+++ backend/crafty_backend/urls.py
@@ -16,15 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
-from market.views import SellerDashboardView
+from market.views import SellerDashboardView, HomeView, AboutView, HelpCenterView, ShippingView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("users.urls")),
-    path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("", HomeView.as_view(template_name="home.html"), name="home"),
+    path('about/', AboutView.as_view(), name="about"),
+    path("help/", HelpCenterView.as_view(), name="help_center"),
+    path("shipping/", ShippingView.as_view(), name="shipping"),
+
     path("seller/dashboard/", SellerDashboardView.as_view(), name="seller_dashboard"),
     path("market/", include("market.urls")),
     path("orders/", include("orders.urls")),
