370cee4b1d60c7be508fc32db78b99c300d2593c
Sonu Kumar <deepakkumar23887@gmail.com>
2025-11-18 18:28:33 +0530

    add seller and category field in product

--- backend/market/models.py
+++ backend/market/models.py
@@ -36,6 +36,8 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, default=Decimal("0.00"))
     stock = models.PositiveIntegerField(default=0)
     is_active = models.BooleanField(default=True)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
