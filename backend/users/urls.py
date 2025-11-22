f58bdc0f4425c1a3726755946404a446c3a3fbb4
Sonu Kumar <deepakkumar23887@gmail.com>
2025-11-22 12:03:19 +0530

    add profile edit path

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import SignUpView, LoginView, LogoutView, ProfileView
+from .views import UserProfileEditForm
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("profile/", ProfileView.as_view(), name="profile"),
+    path("profile/edit", UserProfileEditForm.as_view(), name="edit_profile")
 ]
